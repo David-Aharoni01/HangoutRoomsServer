ed483fe4dff66930b77e516098a61dd9f597afa7
David_Aharoni <david.aharoni01@gmail.com>
2023-01-23 00:38:48 +0200

    changed full http returns to substrings of relevant stuff

--- main.py
+++ main.py
@@ -10,13 +10,8 @@ import time
 import hashlib
 from fastapi.responses import Response
 from pydantic.annotated_types import Any
-import platform
-
 import HelperDB
 
-ip = "tamir's_server"
-ip_testing = "david.lan"
-testing = platform.node() == "HOME"
 port = 8080
 app = FastAPI()
 
@@ -37,7 +32,7 @@ class User(BaseModel):
         if self.profile_picture:
             with open(f"files/profile_pictures/{self.phone_number}.{self.profile_picture.type}", 'xb') as f:
                 f.write(base64.b64decode(self.profile_picture.base_64))
-            self.profile_picture = f"http://{ip}:{port}/files/profile_pictures/{self.phone_number}.{self.profile_picture.type}"
+            self.profile_picture = f"/files/profile_pictures/{self.phone_number}.{self.profile_picture.type}"
 
     def __iter__(self):
         yield self.phone_number
@@ -101,7 +96,7 @@ async def upload(directory: str, file: File):
     except Exception as e:
         return {"message": str(e)}
     return {"message": f"Successfully uploaded file",
-            "url": f"http://{ip}:{port}/files/{directory}/{file_name}"}
+            "url": f"/files/{directory}/{file_name}"}
 
 
 @app.get("/files/{directory}/{file_name}")
@@ -123,6 +118,4 @@ if __name__ == "__main__":
     conn = HelperDB.create_connection("files/users.db")
     HelperDB.create_users_table(conn)
     conn.close()
-    if testing:
-        ip = ip_testing
-    uvicorn.run(app, host="0.0.0.0", port=port)
+    uvicorn.run(app, host='0.0.0.0', port=port)
